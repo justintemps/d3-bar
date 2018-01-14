b88b7c976eb4eb611a3f8c0ee885225a0ab4fe6f
justintemps <jesmith627@gmail.com>
2018-01-14 01:05:41 +0100

    Add yAxis and bars!

--- bar-chart.js
+++ bar-chart.js
@@ -5,7 +5,7 @@ const height = 600;
 const margin = {
   top: 20,
   bottom: 20,
-  left: 20,
+  left: 40,
   right: 20
 };
 
@@ -37,17 +37,47 @@ d3.csv('data.csv', (err, res) => {
     return false;
   });
 
+  const yMax = d3.max(data, d => d.total);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, yMax])
+    .range([height - margin.bottom, margin.top]);
+
+  const heightScale = d3
+    .scaleLinear()
+    .domain([0, yMax])
+    .range([0, height - margin.top - margin.bottom]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+
   const xScale = d3
     .scaleBand()
     .domain(data.map(d => d.country))
     .range([margin.left, width - margin.right]);
 
-  const xAxis = d3
-    .axisBottom()
-    .scale(xScale);
+  const xAxis = d3.axisBottom().scale(xScale);
 
   svg
     .append('g')
     .attr('transform', `translate(${[0, height - margin.bottom]})`)
     .call(xAxis);
+  svg
+    .append('g')
+    .attr('transform', `translate(${[margin.left, 0]})`)
+    .call(yAxis);
+
+  const barWidth = (width - margin.right - margin.left) / data.length;
+
+  const rect = svg
+    .selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('width', barWidth - 15)
+    .attr('height', d => heightScale(d.total))
+    .attr('x', d => xScale(d.country))
+    .attr('y', d => yScale(d.total))
+    .attr('fill', 'blue')
+    .attr('stroke', 'white');
 });
